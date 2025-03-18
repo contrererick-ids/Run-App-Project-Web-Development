b247b9426223d689c63183bd7b5e5ee6604022dd
Salvador Rodriguez <chava.crb@gmail.com>
2025-03-17 18:01:53 -0600

    Add link to Profile page in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import RunningCalendar from "../components/RunningCalendar";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
       <div className=" m-10  border-b-4 border-sky-400 pb-4 flex flex-col items-center">
-        <div className="w-50 h-50 rounded-full bg-neutral-400 items-center flex justify-center">
+        <Link to={"/Profile"} className="w-50 h-50 rounded-full bg-neutral-400 items-center flex justify-center">
           <img
             className="w-40 h-40 rounded-full"
             src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
             alt=""
           />
-        </div>
+        </Link>
         <h1 className="text-4xl font-bold text-start text-neutral-50 mt-10">
           Welcome to Run-App
         </h1>
